455d38f8aa1d4ed05a0c59a16a4b5418fc01dda4
Corey Keller <corey@clear-measure.com>
2017-12-18 09:48:41 -0500

    formating

--- Day4.fs
+++ Day4.fs
@@ -1,5 +1,6 @@
 let i = "oaoe rxeq vssdqtu xrk cjv yaoqp loo
 mveua dogbam szydvri hyzk lbega abzqw xwjn wniug kwbre"
+
 let input = i.Split '\n'
             |> Array.map (fun x ->
                 x.Split ' ')
@@ -8,4 +9,5 @@ let filterFunc elem =
     let a = Array.length elem
     let b = Array.distinct elem |> Array.length
     a = b
+
 let f = Array.filter filterFunc input |> Array.length
